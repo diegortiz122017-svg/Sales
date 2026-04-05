710c3ccb550b6391c5d3d6a7792dc43ecd063b08
diegortiz122017-svg <diegortiz122017@gmail.com>
2026-04-05 17:57:47 -0400

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,6 @@ const express  = require('express');
 const router   = express.Router();
 const crypto   = require('crypto');
 const { body, query, param, validationResult } = require('express-validator');
-const nodemailer = require('nodemailer');
 const { Resend } = require('resend');
 const { fetchFromVAuto, getVehicleById } = require('../config/vauto');
 const { contactLimiter, sanitize } = require('../middleware/security');
